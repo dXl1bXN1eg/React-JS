47590ab1d4cf0c51c38ee4fb2e0c2c236ccaca5e
cyilmaz <cydepo@gmail.com>
2025-01-10 00:54:01 +0300

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  let a = 10;
+  let b = 4;
+
+  let sonuc = true;
+
+  let isimler = [
+    "Hacker",
+    "Siyah Şapka",
+    "Beyaz Şapka",
+    "Gri Şapka"
+  ]
+
+  return (
+    <div style={{ backgroundColor: 'red', height: '100hz' }}>
+      {/* sonuc ? <p>Ehliyeti Alabilirsiniz ..</p> : <p>Ehliyeti Alamassın ..</p> */}
+
+      {/* (a + b) / 2 >= 50 ? <p>Dersten Geçtin</p> : <p>Dersten Kaldın</p> */}
+
+      {isimler.map((isim, index) => (
+        <div style={{ color: 'black' }} key={index}>
+          <a style={{ color: 'black', textDecoration: 'none', fontSize: '20px' }} href='#'>{index}. {isim}</a>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+
+export default App
