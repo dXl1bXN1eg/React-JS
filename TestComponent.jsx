96cd7d8855cca7ee5cf866f43d402d3726b9f402
cyilmaz <cydepo@gmail.com>
2025-01-27 00:08:54 +0300

    Update TestComponent.jsx

--- TestComponent.jsx
+++ TestComponent.jsx
@@ -1,26 +1,34 @@
-import React from 'react'
+import React from 'react';
 
+function TestComponent(props) {
+    const users = [
+        {
+            username: "Ali",
+            password: "parola"
+        },
+        {
+            username: "Ahmet",
+            password: "parola2"
+        }
+    ];
 
-export const users = [
-    {
-        username: "Ali",
-        password: "parola"
-    },
-    {
-        username: "Ahmet",
-        password: "parola2"
-    }
-]
-
-
-function TestComponent() {
     return (
         <div className='Header'>
             <div>
-                Burası Header Alanı
+                Burası Demo Proje Alanı
+            </div>
+
+            <div>
+                Kullanıcılar:
+                {users.map((user, index) => (
+                    <div key={index}>
+                        <div>Username: {user.username}</div>
+                        <div>Password: {user.password}</div>
+                    </div>
+                ))}
             </div>
         </div>
-    )
+    );
 }
 
-export default TestComponent
+export default TestComponent;
