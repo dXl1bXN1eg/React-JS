50f148de2a8df38c008aed7e89a634d6f50de7e9
cyilmaz <cydepo@gmail.com>
2025-01-28 17:54:34 +0300

    Update Login.jsx

--- Login.jsx
+++ Login.jsx
@@ -1,20 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Headers from './Headers';
 import '../css/login.css';
 
 export default function Login() {
+    const [formData, setFormData] = useState({ username: '', password: '' });
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prev) => ({
+            ...prev,
+            [name]: value,
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (formData.username === 'admin' && formData.password === '12345') {
+            alert('Login successful!');
+        } else {
+            alert('Invalid username or password');
+        }
+    };
+
     return (
         <div>
             <Headers />
             <div className='container_login'>
                 <center>
-                    <h1>Login</h1>
-                    <form>
+                    <h1 id='loginname'>Login</h1>
+                    <form onSubmit={handleSubmit}>
                         <label htmlFor="username">Username:</label>
-                        <input type="text" id="username" name="username" required />
+                        <input
+                            type="text"
+                            id="username"
+                            name="username"
+                            value={formData.username}
+                            onChange={handleInputChange}
+                            required
+                        />
 
                         <label htmlFor="password">Password:</label>
-                        <input type="password" id="password" name="password" required />
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            value={formData.password}
+                            onChange={handleInputChange}
+                            required
+                        />
 
                         <input type="submit" value="Login" />
                     </form>
