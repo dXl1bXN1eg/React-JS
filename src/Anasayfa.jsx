54bdb90f169863d650e2295c91a281c4d2112175
cyilmaz <cydepo@gmail.com>
2025-01-31 17:38:51 +0300

    Create Anasayfa.jsx

---
+++ src/Anasayfa.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/anaekran.css';
+import screen from '../images/screen.png';
+
+
+export default function Anasayfa() {
+    return (
+        <div className='background-container'>
+            <img src={screen} />
+        </div>
+    )
+}
